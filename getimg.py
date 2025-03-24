0fc49fe853cd432aa24582ee7caa47b611b4df35
O.o ？ <140151517+SomebodySe@users.noreply.github.com>
2025-03-24 20:43:43 +0800

    Update getimg.py

--- getimg.py
+++ getimg.py
@@ -4,17 +4,13 @@ import aiohttp
 import aiofiles
 import os
 
-# 指定要监听的群组ID
-TARGET_GROUP_ID = 123456789
-
 # 定义一个处理群消息的处理器，仅监听指定群组
 save_emoticon = on_message(priority=5, block=False)
 
 @save_emoticon.handle()
 async def handle_message(bot: Bot, event: GroupMessageEvent):
     # 检查是否为指定的群组
-    if event.group_id != TARGET_GROUP_ID:
-        return
+    group_id = event.group_id
 
     msg = event.message
 
@@ -23,5 +19,6 @@ async def handle_message(bot: Bot, event: GroupMessageEvent):
         if seg.type == "image":
             # 获取图片URL
             image_url = seg.data["url"]
-            with open('src/plugins/image.txt', 'w', encoding='utf-8') as file:
+            filename = f"src/plugins/imageadd/{group_id}.txt"
+            with open(filename, 'w') as file:
                 file.write(image_url)
