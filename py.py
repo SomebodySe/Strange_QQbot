a86c9d94a57f81dbc6a8d297518cd28194830862
O.o ？ <140151517+SomebodySe@users.noreply.github.com>
2025-03-26 16:17:58 +0800

    Update py.py

--- py.py
+++ py.py
@@ -5,8 +5,10 @@ def py(msgx):
     filename = "src/plugins/run.txt"
     with open(filename, 'w', encoding='utf-8') as file:
         file.write(msg)
-    result = subprocess.check_output(["python3", filename], text=True).strip()
+    result = subprocess.check_output(["python3", filename], text=True, timeout=5).strip()
     if len(result) > 1000:
         return result[-1000:]
+    elif result == "":
+        return "无输出"
     else:
         return result
