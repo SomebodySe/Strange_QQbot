7119165abc1c43b282a99352528d930ff1dfcb35
O.o ？ <140151517+SomebodySe@users.noreply.github.com>
2025-03-26 16:21:47 +0800

    Update py.py

--- py.py
+++ py.py
@@ -5,7 +5,10 @@ def py(msgx):
     filename = "src/plugins/run.txt"
     with open(filename, 'w', encoding='utf-8') as file:
         file.write(msg)
-    result = subprocess.check_output(["python3", filename], text=True, timeout=5).strip()
+    try:
+        result = subprocess.check_output(["python3", filename], text=True, timeout=5).strip()
+    except subprocess.TimeoutExpired:
+        return("运行超时")
     if len(result) > 1000:
         return result[-1000:]
     elif result == "":
