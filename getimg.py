45f51ae1032aba34a6e38cff8348cc8f21b04aef
O.o ？ <140151517+SomebodySe@users.noreply.github.com>
2025-05-11 18:01:26 +0800

    Update getimg.py

--- getimg.py
+++ getimg.py
@@ -1,7 +1,8 @@
 from nonebot import on_message
-from nonebot.adapters.onebot.v11 import Bot, GroupMessageEvent
-import aiohttp
-import aiofiles
+from nonebot.adapters.onebot.v11 import Bot, GroupMessageEvent, MessageSegment
+from src.plugins import imageadd
+import requests
+import imghdr
 import os
 
 # 定义一个处理群消息的处理器，仅监听指定群组
@@ -19,6 +20,14 @@ async def handle_message(bot: Bot, event: GroupMessageEvent):
         if seg.type == "image":
             # 获取图片URL
             image_url = seg.data["url"]
-            filename = f"src/plugins/imageadd/{group_id}.txt"
-            with open(filename, 'w') as file:
-                file.write(image_url)
+            if imageadd.imageadd(image_url,group_id):
+                content = requests.get(image_url).content
+                ext = imghdr.what(None, content)
+                save_path = f"src/plugins/imageadd/addimage.{ext}"
+                with open(save_path, 'wb') as f:
+                    f.write(content)
+                image_path = os.path.abspath(f"src/plugins/imageadd/addimage.{ext}")
+                await bot.send_group_msg(group_id=group_id, message=MessageSegment.image(f"file://{image_path}"))
+                print("图片相同")
+            else:
+                print("图片不同")
