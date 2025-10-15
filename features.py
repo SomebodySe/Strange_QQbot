ad432228c83ecf2de041f979666d9909aa42108c
O.o ？ <140151517+SomebodySe@users.noreply.github.com>
2025-10-15 15:57:31 +0800

    Update and rename caidan.py to features.py

--- features.py
+++ features.py
@@ -1,9 +1,10 @@
-def caidan():
+def features():
     try:
-        with open("src/plugins/caidan.txt", 'r') as file:
+        with open("src/plugins/features.txt", 'r') as file:
             content = file.read()
         return content  # 输出文件内容
     except FileNotFoundError:
         return f"文件不存在。"
+
 
 # print(caidan())
