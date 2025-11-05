a99ae11d3bc3e1d379166d03fae67bc5905f2be9
O.o ？ <140151517+SomebodySe@users.noreply.github.com>
2025-11-05 12:08:34 +0800

    Update ai.py

--- ai.py
+++ ai.py
@@ -5,7 +5,7 @@ import os
 
 # 你的 DeepSeek API Key
 def ai(msgx, group_id, user_id, ischat):
-    API_KEY = "sk-2961bc7f08c8456ca997c158edd366b4"  # 替换为你的 API Key
+    API_KEY = "sk-xxx"  # 替换为你的 API Key
     client = OpenAI(api_key=API_KEY, base_url="https://api.deepseek.com/v1")
     
     mode = "deepseek-chat"
